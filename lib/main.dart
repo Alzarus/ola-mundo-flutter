16ba5b6c704dc32b2f46ea149776951bfb4e28d2
Pedro <alzarus.25@gmail.com>
2023-10-09 00:24:54 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const AppWidget(title: 'Xaminhaneném'),
+  );
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(title,
+          textDirection: TextDirection.ltr,
+          style: const TextStyle(color: Colors.white, fontSize: 40.0)),
+    );
+  }
+}
